c73f818078fc1293e791f5efcb76bd5e32a85354
Martyn Clifford <m.a.clifford2@gmail.com>
2026-07-15 10:21:38 +0100

    Add project files.

---
+++ BoxEditor/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <BoxEngine.h>
+
+int main() {
+	std::cout << "Hello, BoxEditor!" << std::endl;
+
+	testFunction();
+
+	return 0;
+}
---
+++ BoxEngine/src/BoxEngine.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "BoxEngine.h"
+
+// TODO: This is an example of a library function
+void testFunction()
+{
+	std::cout << "This is a test function from BoxEngine lib!" << std::endl;
+}
